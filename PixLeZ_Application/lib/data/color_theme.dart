098541701d0fe5e41e8f3f3e1d0a3344e59f6a45
TheRisenPhoenix <43245438+TheRisenPhoenix@users.noreply.github.com>
2022-03-30 16:24:15 +0200

    Fix warnings

--- PixLeZ_Application/lib/data/color_theme.dart
+++ PixLeZ_Application/lib/data/color_theme.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:hive/hive.dart';
 
 import 'package:uuid/uuid.dart';
--- PixLeZ_Application/lib/main.dart
+++ PixLeZ_Application/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 import 'package:PixLeZ/data/state_notifier.dart';
